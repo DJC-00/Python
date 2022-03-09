9b152dddab646219149e9a15178e4835bdad73c7
DJC-00 <djcastle2172@gmail.com>
2022-03-09 17:07:50 -0600

    understanding Routing

--- Flask/fundamentals/routing/server.py
+++ Flask/fundamentals/routing/server.py
@@ -10,15 +10,27 @@ def dojo():
 
 @app.route('/say/<input>')
 def saySomething(input):
+    for i in range(len(input)):
+            if (str.isdigit(input[i])):
+                return "Invalid Input: Input argument contains a number "
     return (f"Hi {input}")
 
 @app.route('/repeat/<num>/<text>')
 def repeat(num,text):
-    endStr = ""
-    text = text + " "
-    for i in range (int(num)):
-        endStr += text
-    return endStr
+    if (str.isdigit(num)):
+        for i in range(len(text)):
+            if (str.isdigit(text[i])):
+                return "Invalid Input: Second argument contains a number "
+        endStr = ""
+        text = text + " "
+        for i in range (int(num)):
+            endStr += text
+        return endStr
+    return (f"invalid Input: First argument is not a number")
+
+@app.route('/<route>')
+def invalid(route):
+    return "This is not a valid path"
 
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)  
